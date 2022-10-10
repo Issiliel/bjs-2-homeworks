9bbeb9faad3c6a00fe73464709a254bf49ac7c57
Elena Voronina <voronina013@gmail.com>
2022-10-10 14:55:00 +0300

    first solution

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash == hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    }
+
+    let result = func(...args);
+    cache.push({
+      'hash': hash,
+      'value': result
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисления: " + result);
+    return "Вычисления: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer = null;
+
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+  }
+  return wrapper;
+}
+
+function debounceDecorator2(func) {
+  let timer = null;
+
+  function wrapper(...args) {
+    if (timer === null) {
+      func(...args);
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    wrapper.count++;
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
